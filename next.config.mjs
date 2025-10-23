ace9925150c10aa7b5578abb67c22c9a52a20497
Gustavo Amezcua <gusta2897@gmail.com>
2025-10-23 15:13:03 -0700

    Added folders and files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ utils/authOptions.js
@@ -0,0 +1,2 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
