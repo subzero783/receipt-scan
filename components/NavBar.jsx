ce51d7f5c48ccf5790938458feee33b626abfedd
Gustavo Amezcua <gusta2897@gmail.com>
2025-11-11 20:16:09 -0800

    RSSCN-17 Creating NavBar.jsx

---
+++ components/NavBar.jsx
@@ -0,0 +1,122 @@
+"use client";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
+import profileDefault from "@/assets/images/profile.png";
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "@/assets/images/company-logo.png";
+
+const Navbar = () => {
+  const { data: session } = useSession();
+  const profileImage = session?.user?.image;
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+
+  const pathName = usePathname();
+
+  return (
+    <nav className="top-navigation">
+      <Container>
+        <Row>
+          <Col>
+            {/* Mobile Menu Button */}
+            <button
+              type="button"
+              id="mobile-dropdown-button"
+              aria-controls="mobile-menu"
+              aria-expanded={isMobileMenuOpen}
+              onClick={() => setIsMobileMenuOpen((prev) => !prev)}
+            >
+              <span></span>
+              <span className="sr-only">Open main menu</span>
+              <svg
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </button>
+          </Col>
+          <Col className="logo-column">
+            <Link href="/">
+              <Image
+                src={logo}
+                alt=""
+              />
+            </Link>
+          </Col>
+          <Col>
+            <Link
+              href="/"
+              className={`${pathName === "/" ? "active" : ""}`}
+            >
+              Home
+            </Link>
+            <Link
+              href="/blog"
+              className={`${pathName === "/blog" ? "active" : ""}`}
+            >
+              Blog
+            </Link>
+            <Link
+              href="/about"
+              className={`${pathName === "/about" ? "active" : ""}`}
+            >
+              About
+            </Link>
+            <Link
+              href="/features"
+              className={`${pathName === "/features" ? "active" : ""}`}
+            >
+              Features
+            </Link>
+            <Link
+              href="/pricing"
+              className={`${pathName === "/pricing" ? "active" : ""}`}
+            >
+              Pricing
+            </Link>
+          </Col>
+          {/* Right Side Menu */}
+          {session && (
+            <Col className="right-side-menu-col">
+              {/* Profile Image */}
+              <div className="profile-image-container">
+                <button
+                  type="button"
+                  id="user-menu-button"
+                  aria-expanded={isProfileMenuOpen}
+                  aria-haspopup="true"
+                  onClick={() => setIsProfileMenuOpen((prev) => !prev)}
+                >
+                  <span></span>
+                  <span className="sr-only">Open user menu</span>
+                  <Image
+                    src={profileImage || profileDefault}
+                    alt=""
+                    width={40}
+                    height={40}
+                  />
+                </button>
+              </div>
+              {/* Profile Dropdown Menu */}
+              <div className="profile-dropdown-menu-container"></div>
+            </Col>
+          )}
+        </Row>
+      </Container>
+    </nav>
+  );
+};
+
+export default Navbar;
