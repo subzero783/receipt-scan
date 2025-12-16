dfaed0360b41b6901f0218eff57262938fcab03f
Gustavo Amezcua <gusta2897@gmail.com>
2025-12-16 11:02:57 -0800

    RSSCN-20 Finished Hero Section Desktop version

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/assets/images/company-logo.png";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <section className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Link href="/">
+              <Image
+                src={logo}
+                alt=""
+              />
+            </Link>
+          </div>
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
