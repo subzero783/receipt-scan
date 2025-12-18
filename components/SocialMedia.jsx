51112d0a79d4de1259b7747b2149e4d9ab5e4c27
subzero783 <gusta2897@gmail.com>
2025-12-18 23:28:12 +0000

    RSSCN-23 Finished Footer.jsx Desktop version

--- components/SocialMedia.jsx
+++ components/SocialMedia.jsx
@@ -1,7 +1,17 @@
 import Link from "next/link"
+import { FaFacebook, FaInstagram, FaLinkedin, FaTiktok } from "react-icons/fa";
+
+
 
 const SocialMedia = () => {
-    return(<></>)
+    return(
+        <div className="social-media-links">
+            <Link href="/"><FaFacebook/></Link>
+            <Link href="/"><FaInstagram/></Link>
+            <Link href="/"><FaLinkedin/></Link>
+            <Link href="/"><FaTiktok/></Link>
+        </div>
+    )
 }
 
 export default SocialMedia;
