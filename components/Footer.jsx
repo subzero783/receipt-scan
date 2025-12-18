a25e40b496e28c42c00b23c2a327c2c385517381
subzero783 <gusta2897@gmail.com>
2025-12-18 19:54:20 +0000

    RSSCN-23 Working on footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,35 +7,37 @@ const Footer = () => {
     <section className="footer">
       <div className="container">
         <div className="row">
-          <div className="col">
-            <Link href="/">
-              <Image
-                src={logo}
-                alt=""
-              />
-            </Link>
-          </div>
-          <div className="col">
-            <h3>Product</h3>
-            <div className="links-container">
-              <Link href="/features">Features</Link>
-              <Link href="/pricing">Pricing</Link>
-              <Link href="/blog">Blog</Link>
+          <div className="col links-columns">
+            <div className="col">
+              <Link href="/">
+                <Image
+                  src={logo}
+                  alt=""
+                />
+              </Link>
             </div>
-          </div>
-          <div className="col">
-            <h3>Company</h3>
-            <div className="links-container">
-              <Link href="/about-us">About Us</Link>
-              <Link href="/contact">Contact</Link>
+            <div className="col">
+              <h3>Product</h3>
+              <div className="links-container">
+                <Link href="/features">Features</Link>
+                <Link href="/pricing">Pricing</Link>
+                <Link href="/blog">Blog</Link>
+              </div>
             </div>
-          </div>
-          <div className="col">
-            <h3>Legal</h3>
-            <div className="links-container">
-              <Link href="/terms">Terms</Link>
-              <Link href="/privacy">Privacy</Link>
-              <Link href="/cookies">Cookies</Link>
+            <div className="col">
+              <h3>Company</h3>
+              <div className="links-container">
+                <Link href="/about-us">About Us</Link>
+                <Link href="/contact">Contact</Link>
+              </div>
+            </div>
+            <div className="col">
+              <h3>Legal</h3>
+              <div className="links-container">
+                <Link href="/terms">Terms</Link>
+                <Link href="/privacy">Privacy</Link>
+                <Link href="/cookies">Cookies</Link>
+              </div>
             </div>
           </div>
           <div className="col">
