de194cbbaec74dde7bc88d6bbd9fd5e093a71da9
Gustavo Amezcua <gusta2897@gmail.com>
2025-12-18 20:03:38 -0800

    Working on HowItWorks.jsx

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroSection";
+import HowItWorks from "@/components/HowItWorks";
 
 export default async function Home() {
   return (
     <div className="home-page">
       <HeroSection />
+      <HowItWorks />
     </div>
   );
 }
--- components/HowItWorks.jsx
+++ components/HowItWorks.jsx
@@ -1,5 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import siteData from "@/data/siteData.json";
+
 const HowItWorks = () => {
-  return <div>HowItWorks</div>;
+  return (
+    <section className="how-it-works">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="boxes">
+              {siteData[1].how_it_works.boxes.map((item, index) => (
+                <div
+                  className="box"
+                  key={index}
+                >
+                  <Image
+                    src={`/images${item.image}`}
+                    alt={item.title}
+                    width={100}
+                    height={100}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HowItWorks;
