3445e1de403e5b2492d27fb8e05f0d19eb3e08e3
subzero783 <gusta2897@gmail.com>
2025-12-20 23:20:45 +0000

    RSSCN-25 Working on Features.jsx

--- components/Features.jsx
+++ components/Features.jsx
@@ -7,7 +7,9 @@ const Features = () => {
         <section className="features">
             <div className="container">
                 <div className="row">
-                    <div className="col"></div>
+                    <div className="col">
+                        
+                    </div>
                 </div>
             </div>
         </section>
