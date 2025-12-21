028ce8e653fe98103aff56ac237adf9e204a8882
subzero783 <gusta2897@gmail.com>
2025-12-21 20:40:24 +0000

    RSSCN-25 Working on Features.jsx

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection";
 import HowItWorks from "@/components/HowItWorks";
+import Features from "@/components/Features";
 
 export default async function Home() {
   return (
     <div className="home-page">
       <HeroSection />
       <HowItWorks />
+      <Features />  
     </div>
   );
 }
--- components/Features.jsx
+++ components/Features.jsx
@@ -1,18 +1,39 @@
 import Link from "next/link";
 import { MdKeyboardArrowRight } from "react-icons/md";
+import { FaCube } from "react-icons/fa6";
 import siteData from "@/data/siteData.json";
 
 const Features = () => {
     return(
-        <section className="features">
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        
-                    </div>
-                </div>
+    <section className="features">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="top-text">
+              <p className="small-title">{siteData[2].features.small_title}</p>
+              <h2 className="title">{siteData[2].features.title}</h2>
+              <p className="subtitle">{siteData[2].features.subtitle}</p>
             </div>
-        </section>
+            <div className="boxes">
+              {siteData[2].features.boxes.map((item, index) => (
+                <div
+                  className="box"
+                  key={index}
+                  style={{backgroundImage: `url(/images${item.image})`}}
+                >
+                  <div className="box-text-container">
+                    <FaCube/>
+                    <p className="title">{item.title}</p>
+                    <p className="text">{item.text}</p>
+                    <Link href={item.link.url}>{item.link.text} <MdKeyboardArrowRight/></Link>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     )
 }
 
