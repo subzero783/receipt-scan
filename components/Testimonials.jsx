ddc782e7e56d076c11168abb91726d576b2630dd
subzero783 <gusta2897@gmail.com>
2025-12-21 21:30:22 +0000

    RSSCN-26 Working on Testimonials

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -1,19 +1,27 @@
 import Image from "next/image";
+import siteData from "@/data/siteData.json";
 
 const Testimonials = () => {
     return(
-            <section className="how-it-works">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <div className="top-text">
-              <h2 className="title">{siteData[1].home_page.testimonials.title}</h2>
-              <p className="subtitle">{siteData[1].home_page.testimonials.subtitle}</p>
+        <section className="testimonials">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <div className="top-text">
+                            <h2 className="title">{siteData[1].home_page.testimonials.title}</h2>
+                            <p className="subtitle">{siteData[1].home_page.testimonials.subtitle}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="boxes">
+                    {
+                        siteData[1].home_page.testimonials.boxes.map((item, index)=>(
+                            <div className="box" key={index}></div>
+                        ))
+                    }
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
+        </section>
     )
 }
 
