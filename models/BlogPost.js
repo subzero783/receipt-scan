2128d7946725c3c8c6eaca0f79c34a2b4869097e
Gustavo Amezcua <gusta2897@gmail.com>
2025-12-30 16:32:27 -0800

    RSSCN-43 Working on BlogPost Schema and adding documents to the blogposts collection in MongoDB

---
+++ models/BlogPost.js
@@ -0,0 +1,56 @@
+import { Schema, model, models } from "mongoose";
+
+const BlogPostSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    categories: [
+      {
+        type: String,
+      },
+    ],
+    excerpt: {
+      type: String,
+      required: true,
+    },
+    author: {
+      required: true,
+      name: {
+        type: String,
+      },
+      email: {
+        type: String,
+      },
+    },
+    featured_image: {
+      type: String,
+    },
+    is_featured: {
+      type: Boolean,
+      default: false,
+    },
+    content: {
+      type: String,
+    },
+    status: {
+      required: true,
+      type: String,
+      default: "Draft",
+      enum: ["Draft", "Published"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const BlogPost = models.BlogPost || model("BlogPost", BlogPostSchema);
+
+export default BlogPost;
