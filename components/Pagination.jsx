ea970fac0df466e94e4f10483955a4f90b4edd0d
Gustavo Amezcua <gusta2897@gmail.com>
2026-01-01 17:36:17 -0800

    RSSCN-45 Finished Styling Single Post Template

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -9,25 +9,23 @@ const Pagination = ({ page, pageSize, totalItems, onPageChange }) => {
 
   return (
     <section className="pagination">
-      <div className="container">
-        <button
-          className=""
-          disabled={page === 1}
-          onClick={() => handlePageChange(page - 1)}
-        >
-          Previous
-        </button>
-        <span className="">
-          Page {page} of {totalPages}
-        </span>
-        <button
-          className=""
-          disabled={page === totalPages}
-          onClick={() => handlePageChange(page + 1)}
-        >
-          Next
-        </button>
-      </div>
+      <button
+        className="btn btn-primary"
+        disabled={page === 1}
+        onClick={() => handlePageChange(page - 1)}
+      >
+        Previous
+      </button>
+      <span className="pagination__span">
+        Page {page} of {totalPages}
+      </span>
+      <button
+        className="btn btn-primary"
+        disabled={page === totalPages}
+        onClick={() => handlePageChange(page + 1)}
+      >
+        Next
+      </button>
     </section>
   );
 };
