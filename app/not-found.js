0c38c4bd48154c2359d2d0e0f06148a63ed9e95b
Gustavo Amezcua <gusta2897@gmail.com>
2026-02-19 13:46:16 -0800

    RSSCN-79 Working on the styling of the Not Found page

--- app/not-found.js
+++ app/not-found.js
@@ -3,18 +3,20 @@ import { FaExclamationTriangle } from "react-icons/fa";
 
 const NotFoundPage = () => {
   return (
-    <section className="not-found-section">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <FaExclamationTriangle />
-            <h1>Page Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
-            <Link href="/">Go Home</Link>
+    <div className="not-found-page">
+      <section className="not-found-section">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <FaExclamationTriangle />
+              <h1>Page Not Found</h1>
+              <p>The page you are looking for does not exist.</p>
+              <Link href="/">Go Home</Link>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
