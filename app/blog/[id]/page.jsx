9ae686821750c89a47ba0cae03b4a6b039f9f90f
Gustavo Amezcua <gusta2897@gmail.com>
2026-02-27 18:58:51 -0800

    RSSCN-59 Added different styling to different pages. Need to work on the styling of the remaining pages and then start testing

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -30,11 +30,11 @@ const SingleBlogPage = async ({ params }) => {
       {/* Hero Section */}
       <section className="hero-section">
         <div className="container">
-          <Link href="/blog" className="back-link">
-            <IoIosArrowBack /> All posts
-          </Link>
 
           <div className="post-header-content">
+            <Link href="/blog" className="back-link">
+              <IoIosArrowBack /> All posts
+            </Link>
             {post.categories && post.categories.length > 0 && (
               <div className="categories">
                 {post.categories.map((category, index) => (
@@ -71,35 +71,33 @@ const SingleBlogPage = async ({ params }) => {
 
       {/* Content Section */}
       <section className="content-section">
-        <div className="container content-container">
+        <div className="content-container">
           {/* <h2 className="introduction-header">Introduction</h2> */}
-          <div
-            className="post-content"
-            dangerouslySetInnerHTML={{ __html: post.content }}
-          />
+          <div className="container">
 
-          <div className="post-footer">
-            {/* Conclusion Header if not in content */}
-            {/* <h2 className="introduction-header">Conclusion</h2> */}
-            {/* <p className="post-content">
-              We hope this article has provided valuable insights into efficient expense tracking.
-              Proper management of your finances is key to freelance success.
-            </p> */}
+            <div
+              className="post-content"
+              dangerouslySetInnerHTML={{ __html: post.content }}
+            />
+          </div>
 
-            <div className="bottom-share-container">
-              <div className="share-wrapper">
-                <ShareButtons title={post.title} url={`${process.env.NEXTAUTH_URL || ''}/blog/${post._id}`} />
-              </div>
-
-              {post.categories && post.categories.length > 0 && (
-                <div className="bottom-tags">
-                  {post.categories.map((category, index) => (
-                    <span key={index} className="tag-pill">
-                      {category}
-                    </span>
-                  ))}
+          <div className="container">
+            <div className="post-footer">
+              <div className="bottom-share-container">
+                <div className="share-wrapper">
+                  <ShareButtons title={post.title} url={`${process.env.NEXTAUTH_URL || ''}/blog/${post._id}`} />
                 </div>
-              )}
+
+                {post.categories && post.categories.length > 0 && (
+                  <div className="bottom-tags">
+                    {post.categories.map((category, index) => (
+                      <span key={index} className="tag-pill">
+                        {category}
+                      </span>
+                    ))}
+                  </div>
+                )}
+              </div>
             </div>
 
             {/* Author Bio */}
