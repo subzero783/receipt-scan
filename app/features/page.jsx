d4cb68d51317ca3f97146bebbee2a9f0cca88a17
Gustavo Amezcua <gusta2897@gmail.com>
2026-03-04 15:29:45 -0800

    RSSCN-82 Working on Features page

--- app/features/page.jsx
+++ app/features/page.jsx
@@ -1,5 +1,6 @@
 import siteData from "@/data/siteData.json";
 import HeroSection from "@/components/HeroSection";
+import CallToActionTwo from "@/components/CallToActionTwo";
 
 const FeaturesPage = () => {
 
@@ -8,10 +9,28 @@ const FeaturesPage = () => {
   if (!featuresData) return <div>Loading...</div>;
 
   const hero_section = featuresData.hero_section;
+  const cta = featuresData.cta;
 
   return (
     <div className="features_page">
       <HeroSection data={hero_section} />
+
+      <section className="extraction-description-section">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+
+            </div>
+            <div className="col">
+
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="upload-description-section"></section>
+      <section className="personal-analyst-description-section"></section>
+      <section className="tools-description-section"></section>
+      <CallToActionTwo data={cta} />
     </div>
   )
 };
