cf2b6632f468ca765a84c9c34225f136748e314a
Gustavo Amezcua <gusta2897@gmail.com>
2026-03-05 15:24:20 -0800

    Working on Features page section structure and styling

--- app/features/page.jsx
+++ app/features/page.jsx
@@ -1,6 +1,7 @@
 import siteData from "@/data/siteData.json";
 import HeroSection from "@/components/HeroSection";
 import CallToActionTwo from "@/components/CallToActionTwo";
+import Image from "next/image";
 
 const FeaturesPage = () => {
 
@@ -9,6 +10,10 @@ const FeaturesPage = () => {
   if (!featuresData) return <div>Loading...</div>;
 
   const hero_section = featuresData.hero_section;
+  const extraction_description_section = featuresData.extraction_description_section;
+  const upload_description_section = featuresData.upload_description_section;
+  const personal_analyst_description_section = featuresData.personal_analyst_description_section;
+  const tools_description_section = featuresData.tools_description_section;
   const cta = featuresData.cta;
 
   return (
@@ -18,11 +23,16 @@ const FeaturesPage = () => {
       <section className="extraction-description-section">
         <div className="container">
           <div className="row">
-            <div className="col">
-
+            <div className="col left-column">
+              <div className="content">
+                <h2 className="title">{extraction_description_section.title}</h2>
+                <p className="description">{extraction_description_section.subtitle}</p>
+              </div>
             </div>
-            <div className="col">
-
+            <div className="col right-column">
+              <div className="image-wrapper">
+                <Image width={0} height={0} src={extraction_description_section.image} alt={extraction_description_section.image_alt} />
+              </div>
             </div>
           </div>
         </div>
